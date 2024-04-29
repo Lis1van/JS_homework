03f00685ffe2747efb4c8f6e6fb562a42b7f2b0e
Ivan <gameinoriginal@gmail.com>
2024-04-29 22:55:03 +0300

    start less 1

---
+++ lessons-js/lesson-1/less_1.js
@@ -0,0 +1,51 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+
+let a1 = 'Hello';
+let a2 = 'owu';
+let a3 = 'com';
+let a4 = 'ua';
+let b1 = 1;
+let b2 = 10;
+let b3 = -999;
+let b4 = 123;
+let b5 = 3.14;
+let b6 = 2.7;
+let b7 = 16;
+let c1 = true;
+let c2 = false;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(b1);
+console.log(b2);
+console.log(b3);
+console.log(b4);
+console.log(b5);
+console.log(b6);
+console.log(b7);
+console.log(c1);
+console.log(c2);
+
+    // Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
+// (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Litvinenko';
+let middleName = 'Ivan';
+let lastName = 'Sergeevich';
+let result = firstName + ' ' + middleName + ' ' + lastName;
+console.log(result);
+
+    // За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+    // let a = 100; let b = '100'; let c = true;
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//     Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
