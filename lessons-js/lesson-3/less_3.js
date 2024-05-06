ff36a1dbd4a2f277d9bfb570d6298230e6e4dd85
Ivan <gameinoriginal@gmail.com>
2024-05-06 23:13:23 +0300

    start less 3

--- lessons-js/lesson-3/less_3.js
+++ lessons-js/lesson-3/less_3.js
@@ -1,13 +1,45 @@
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+let text = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+for (let i = 0; i < text.length; i++) {
+    document.write(`<div><p>Lorem ipsum dolor sit amet.</p></div>`);
+}
+
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+let text2 = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+let idNumber = 0;
+for (let i of text2) {
+    i.id = idNumber;
+    idNumber = idNumber + 1;
+    document.write(`<div><h2>ID number: ${idNumber}</h2> <p>Lorem ipsum dolor sit amet, consectetur.</p></div>`);
+}
+
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+let text3 = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+let i = 0;
+while (i < text3.length) {
+    document.write(`<div><p>Lorem ipsum dolor sit amet.</p></div>`);
+    i++;
+}
+
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
-//
+
+let text4 = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+let idNumber2 = 1;
+let a = 0;
+while (a < text4.length) {
+    document.write(`<div><h2>ID number: ${idNumber2}</h2> <p>Lorem ipsum dolor sit amet, consectetur.</p></div>`);
+    idNumber2++;
+    a++;
+}
+
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Масив:
 //
-//     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+    let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
 //
 // ШАБЛОН:
 //     <ul>
@@ -21,35 +53,39 @@
 //     </ul>
 //
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
-//
+
+for (let listOfItem = 0; listOfItem < listOfItems.length; listOfItem++) {
+    document.write(`<li>${listOfItems[listOfItem]}</li>`)
+}
+
 // -----------------------------------------------
 //
 //     Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
 // Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
 //
-// let products = [
-//     {
-//         title: 'milk',
-//         price: 22,
-//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-//     },
-//     {
-//         title: 'juice',
-//         price: 27,
-//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-//     },
-//     {
-//         title: 'tomato',
-//         price: 47,
-//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-//     },
-//     {
-//         title: 'tea',
-//         price: 15,
-//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-//     },
-// ];
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
 //
 // ШАБЛОН
 // <div class="product-card">
@@ -57,9 +93,17 @@
 // <img src="IMAGE" alt="" class="product-image">
 // </div>
 // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
-//
-//
-//
+
+for (const product of products){
+    document.write(`
+    <div class="product-card">
+        <h3 class="product-title">${product.title} Price - ${product.price}</h3>
+        <div>
+            <img src="${product.image}" alt = "image" class="product-image" >
+        </div>
+    </div>`)
+}
+
 // --------------------
 //     є масив
 // let users = [
